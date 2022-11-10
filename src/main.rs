e4b49eb46517d696733d78f4c828d932110a7dbb
Ethan Holz <ethan.holz@thoriumworks.com>
2022-11-10 16:24:13 -0500

    feat: Preliminary support for shell completion and better documentation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 mod config;
 use std::env;
+use std::io;
 use std::process;
 
+use clap::Command;
+use clap::CommandFactory;
 use clap::{Args, Parser, Subcommand};
+use clap_complete::generate;
+use clap_complete::Generator;
+use clap_complete::Shell;
 use config::config::{RutConfig, Window};
 use tmux_interface::Sessions;
 use tmux_interface::SESSION_ALL;
@@ -10,26 +16,33 @@ use tmux_interface::{Error, TmuxCommand, TmuxOutput};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
 struct Cli {
     #[arg(short, long)]
+    /// Sets a custom config file
     file: Option<String>,
     #[command(subcommand)]
-    command: Commands,
+    command: Option<Commands>,
+    #[arg(long = "generate", value_enum)]
+    generator: Option<Shell>,
 }
 
-#[derive(Subcommand)]
+#[derive(Subcommand, PartialEq)]
 enum Commands {
+    /// Attaches to a given session
     Attach(SessionCommand),
+    /// Lists sessions in config
     List(List),
+    /// Starts a given session, detached
     Start(SessionCommand),
 }
 
-#[derive(Args)]
+#[derive(Args, PartialEq)]
 struct SessionCommand {
     name: Option<String>,
 }
 
-#[derive(Args)]
+#[derive(Args, PartialEq)]
 struct List {}
 
 fn send_command(window: &Window, tmux: TmuxCommand) -> Result<TmuxOutput, Error> {
@@ -41,15 +54,35 @@ fn send_command(window: &Window, tmux: TmuxCommand) -> Result<TmuxOutput, Error>
     tmux.send_keys().key(&command).key("C-m").output()
 }
 
+fn print_completions<G: Generator>(gen: G, cmd: &mut Command) {
+    generate(gen, cmd, cmd.get_name().to_string(), &mut io::stdout());
+}
+
 fn main() {
     let cli = Cli::parse();
+    if cli.generator == None && cli.file == None && cli.command == None {
+        let mut cmd = Cli::command();
+        cmd.print_help().unwrap();
+        return;
+    }
+    if let Some(generator) = cli.generator {
+        let mut cmd = Cli::command();
+        print_completions(generator, &mut cmd);
+        return;
+    }
     let home = env::var("HOME").unwrap();
     let config_home = format!("{}/.config/ruts/ruts.yaml", home);
     let file = cli.file.as_deref().unwrap_or(&config_home);
-    let data = std::fs::read_to_string(file).expect("Error reading");
+    let data = match std::fs::read_to_string(file) {
+        Ok(data) => data,
+        Err(_) => {
+            println!("Error reading config");
+            return;
+        }
+    };
     let decoded: RutConfig = serde_yaml::from_str(&data).unwrap();
     let workspaces = decoded.workspaces.as_ref().unwrap();
-    match &cli.command {
+    match &cli.command.unwrap() {
         Commands::List(..) => {
             for workspace in workspaces {
                 println!("{}", workspace.name);
